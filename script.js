684360e3807cb676797c729e1c2c40a31ad01b5f
Yure Rafael <112043267+yure07@users.noreply.github.com>
2023-09-16 17:49:23 -0300

    add: transition to load sections

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const abilitiesSection = document.getElementById('abilities-container')
+const projectsSection = document.getElementById('projects-container')
+const experiencesSection = document.getElementById('experiences-container')
+
 const clickSrollTo = (id) => document.getElementById(id).scrollIntoView(id)
+
+document.addEventListener('scroll', () => {
+    if(window.scrollY > 370) abilitiesSection.style.opacity = '1'
+    
+    if(window.scrollY > 1170) projectsSection.style.opacity = '1'
+
+    if(window.scrollY > 1870) experiencesSection.style.opacity = '1'
+})
